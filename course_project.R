59aff3b16c3510798d92c64ba4041458e02275e7
Adrian <isc.montenegro@gmail.com>
2017-09-11 23:50:25 -0500

    Add files via upload

--- course_project.R
+++ course_project.R
@@ -13,14 +13,9 @@ y_test<-read.table("Data_Cleaning/Course_Project/UCI_HAR_Dataset/test/y_test.txt
 #Reading Test Measures
 x_test<-read.table("Data_Cleaning/Course_Project/UCI_HAR_Dataset/test/X_test.txt")
 
-###Creating a 1 column data frame to integrate later to the test data frame. 
-###This columns indicates the type of the data, in this case is test data
-#type_test<-data.frame(rep('test',nrow(x_test)))
-
 #Giving a meaningfull name to the data sets that are going to integrate the Test Data later
 names(subject_test)<- c('Subject')
 names(y_test)<-c('Activity')
-#names(type_test)<-c('Type')
 names(x_test)<-f_names
 #***************************************************************************************
 
@@ -32,37 +27,38 @@ y_train<-read.table("Data_Cleaning/Course_Project/UCI_HAR_Dataset/train/y_train.
 #Reading Test Measures
 x_train<-read.table("Data_Cleaning/Course_Project/UCI_HAR_Dataset/train/X_train.txt")
 
-###Creating a 1 column data frame to integrate later to the training data frame. 
-###This columns indicates the type of the data, in this case is training data
-##type_train<-data.frame(rep('train',nrow(x_train)))
-
 #Giving a meaningfull name to the data sets that are going to integrate the Train Data later
 names(subject_train)<- c('Subject')
 names(y_train)<-c('Activity')
-#names(type_train)<-c('Type')
 names(x_train)<-f_names
 #********************************************************************
 
 #Binding columns to integrate Subject, Activity and Measures for Test and Train Data
-#Consolidating , Subject Test, Activity Test and Test Measures in 1 Test Data Frame
+#Consolidating Subject Test, Activity Test and Test Measures in 1 Test Data Frame
 test_data<-cbind(subject_test,y_test,x_test)
-#Consolidating , Subject Train, Activity Train and Train Measures in 1 Train Data Frame
+#Consolidating Subject Train, Activity Train and Train Measures in 1 Train Data Frame
 train_data<-cbind(subject_train,y_train,x_train)
 
 #Merging Test and Train Data
 all_data<-rbind(test_data, train_data)
 
-
+#Looking for all the mean and standard deviation measures
 mean_var<-all_data[,grepl("mean()",names(all_data))]
 std_var<-all_data[,grepl("std()",names(all_data))]
+
+#Storing subject in a single column data frame
 subject<-data.frame(SBJ=all_data$Subject)
+#Storing activity in a single column data frame and giving a meaningful name
 activity<-data.frame(ACT=all_data$Activity)
-activity$ACT_LBL<-factor(subject$ACT,labels=c("WALKING","WALKING_UPSTAIRS","WALKING_DOWNSTAIRS","SITTING","STANDING","LAYING"))
+activity$ACT_LBL<-factor(activity$ACT,labels=c("WALKING","WALKING_UPSTAIRS","WALKING_DOWNSTAIRS","SITTING","STANDING","LAYING"))
 
-final_set<-cbind(all_data$Subject,activity$ACT_LBL,mean_var, std_var)
+#Creating a dinal data set binding Subject, activity (with meaningful names), and mean & sd measures
+final_set<-cbind(subject$SBJ,activity$ACT_LBL,mean_var, std_var)
 
-names(final_set)[names(final_set)=="all_data$Subject"]<-"Subject"
+#Cleaning the subject and activity column names
+names(final_set)[names(final_set)=="subject$SBJ"]<-"Subject"
 names(final_set)[names(final_set)=="activity$ACT_LBL"]<-"Activity"
 
+#Aggregating to calculate average, grouping by Subject and Activity
 final_set_means<-aggregate(final_set[,3:ncol(final_set)],list(final_set$Subject,final_set$Activity),mean)
 
